1c8ebe1f96c559da59b25b4781f2ec578bf07f62
Ibtihal-mrn <mourninibtihal@gmail.com>
2024-04-18 10:39:26 +0200

    response ok and ping rcv

--- client.py
+++ client.py
@@ -2,29 +2,54 @@
 import socket
 import json
 
+
 host, port = ('127.0.0.1', 3000)
-
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+socket_envoie = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #ce socket sert à envoyer des requêtes 
 try:
-    socket.connect((host, port))
+    socket_envoie.connect((host, port))
     print("Client connecté")
-
-    
+# Dans cette partie, on envoie un message au serveur
     data = json.dumps({
    "request": "subscribe",
-   "port": 3000,
+   "port": 4000, #on précise le port sur lequel le serveur va nous envoyer des requêtes 
    "name": "Ibtihal",
    "matricules": ["22210", "67890"]
     })
     data = data.encode("utf-8")
-    socket.sendall(data) #on envoie notre message sur le port 3000 svia le réseau
-    response = json.loads(socket.recv(2048).decode())
+    socket_envoie.sendall(data) #on envoie notre message sur le port 3000 svia le réseau
+    response = socket_envoie.recv(4096)
+    json.loads(response.decode())
     print(response)
+    socket_envoie.close()
 
-except:
-    print("Connexion au serveur échouée!")
-finally: #on demande de fermer le socket dans tous les cas
-    socket.close()
+
+#Partie de code qui permet de recevoir des requetes, on va donc devoir créer un autre socket
+    
+    socket_reçu = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #cette ligne spécifie le type de socket, TCP communication
+    socket_reçu.bind(('0.0.0.0', 4000)) #on lie le socket au port 4000, c'est sur ce port qu'on va recevoir des messages
+    while True:
+        socket_reçu.listen()
+        conn, address = socket_reçu.accept() #
+        print("En écoute")
+
+
+        ping = json.loads(conn.recv(2048).decode()) #conn pour dire que je recois le message 
+        print(ping)
+
+        #on s'assure que le type du message est un request
+        # if ping.get("type") == "request":
+        #     pong = json.dumps({"response":"pong"}).encode("utf-8")
+        #     socket_envoie.sendall(pong)
+    
+        socket_reçu.close()
+
+# except:
+#     print("Connexion au serveur échouée!")
+
+except Exception as e:
+    print("Connexion au serveur échouée :", e)
+
+
+   
 
     
